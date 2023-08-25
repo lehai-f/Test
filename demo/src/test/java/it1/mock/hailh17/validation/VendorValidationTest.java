9edbc4ec6825ba46b86f037585697cda13815519
lehai-f <lehai9229@gmail.com>
2023-08-25 09:23:05 +0700

    add index page

---
+++ demo/src/test/java/it1/mock/hailh17/validation/VendorValidationTest.java
@@ -0,0 +1,34 @@
+package it1.mock.hailh17.validation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VendorValidationTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSupports() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testValidate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFlag() {
+		fail("Not yet implemented");
+	}
+
+}
